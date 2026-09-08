f7b5a022aed371fa55cd9acb21bd7ae0ee46d88b
M. A. SHARIAR ABID <261-35-022@diu.edu.bd>
2026-09-09 01:24:15 +0600

    Add files via upload

---
+++ basic c++/string to vector.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	// your code goes here
+    
+    string s={"compare"};
+    
+    string terget={"parecom"};
+    
+    vector<char>char_s(s.begin(),s.end());
+    
+    for(auto a:char_s){
+        cout<<a;
+    }
+    
+    
+  
+}
